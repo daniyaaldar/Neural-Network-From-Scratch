2b2e38f09876de4bf64c66a1eebabad3c406bf95
Dani2051 <dardaniyaal@gmail.com>
2025-12-18 03:28:09 +0000

    Implemented Neuron class
    Implemented the basics for neuron class

--- include/Neuron.h
+++ include/Neuron.h
@@ -1,8 +1,37 @@
 #pragma once
 
+#include <vector>
 #include "MathUtility.h"
 
-class Neuron {
+class Neuron 
+{
 public:
+    Neuron(size_t inputsPerNeuron, MathUtility::ActivationFunction activationFunc, double learningRate);
+
+    void print() const;
+    void setOutput(double output) { m_output = output; };
+    void setBias(double bias) { m_bias = bias; };
+    void setDelta(double delta) { m_delta = delta; };
+    void setNumOfInputs(size_t inputsPerNeuron) { m_weights = MathUtility::getRandomData(inputsPerNeuron, -1.0, 1.0); }
+    void setWeights(const std::vector<double>& weights) { m_weights = weights; };
+    
+    double getOutput() const { return m_output; };
+    double getBias() const { return m_bias; };
+    double getDelta() const { return m_delta; };
+    size_t getNumOfInputs() const { return m_weights.size(); };
+    std::vector<double> getWeights() const { return m_weights; };
+
+    void activate(const std::vector<double>& inputs);
+    double activateDerivative(double x) const;
+    std::vector<double> backwardsPropagate(double target);
+    void calculateDelta(double target);
+
 private:
+    std::vector<double> m_weights; // input weights
+    double m_bias;
+    double m_output;
+    double m_delta;
+    double m_learningRate;
+    std::function<double(double)> m_activationFunc;
+    std::function<double(double)> m_activationDerivativeFunc;
 };
--- src/Neuron.cpp
+++ src/Neuron.cpp
@@ -1,1 +1,49 @@
 #include "Neuron.h"
+#include <random>
+#include <iostream>
+
+Neuron::Neuron(size_t inputsPerNeuron, MathUtility::ActivationFunction activationFunc, double learningRate)
+{
+    setNumOfInputs(inputsPerNeuron);
+    m_bias = MathUtility::getRandomData(-10.0, 10.0);
+    m_output = 0.0;
+    m_delta = 0.0;
+    m_activationFunc = MathUtility::getActivationFunc(activationFunc);
+    m_learningRate = learningRate;
+}
+
+void Neuron::print() const
+{
+    std::cout << "OUTPUT: " << m_output << std::endl;
+}
+
+void Neuron::activate(const std::vector<double>& inputs)
+{
+    double total = m_bias;
+
+    if (inputs.size() == m_weights.size())
+    {
+        for (size_t i = 0; i < inputs.size(); i++)
+        {
+            total += inputs[i] * m_weights[i];
+        }
+    }
+
+    m_output = m_activationFunc(total);
+}
+
+double Neuron::activateDerivative(double x) const
+{
+    return x;
+}
+
+std::vector < double > Neuron::backwardsPropagate(double target)
+{
+    std::vector < double > res;
+    return res;
+}
+
+void Neuron::calculateDelta(double target)
+{
+    m_delta = m_output - target;
+}
