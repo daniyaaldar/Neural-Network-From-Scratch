451775190cf6853084b1a48ee0e38dce961baa60
Dani2051 <dardaniyaal@gmail.com>
2025-12-18 03:29:43 +0000

    Implemented Neural Network class
    Implemented basics for Neural Network class (forward pass)

--- include/NeuralNetwork.h
+++ include/NeuralNetwork.h
@@ -3,7 +3,20 @@
 #include "MathUtility.h"
 #include "Layer.h"
 
+#include <vector>
+
 class NeuralNetwork {
 public:
+    NeuralNetwork(const std::vector<size_t>& numOfNeuronsPerLayer, MathUtility::ActivationFunction activationFunc, double learningRate);
+    ~NeuralNetwork();
+
+    void print() const;
+    void feedForward(const std::vector<double>& inputs);
+    void backwardsPropagate(const std::vector<double>& targets);
+    bool setWeights(size_t layerNum, const std::vector<std::vector<double>>& weights);
+    bool setWeights(size_t layerNum, size_t neuronNum, const std::vector<double>& weights);
+    std::vector<double> getOutput() const { return m_layers.back()->getOutputs(); }
+
 private:
+    std::vector<Layer*>  m_layers;
 };
--- src/NeuralNetwork.cpp
+++ src/NeuralNetwork.cpp
@@ -1,1 +1,63 @@
 #include "NeuralNetwork.h"
+#include <iostream>
+
+NeuralNetwork::NeuralNetwork(const std::vector<size_t>& numOfNeuronsPerLayer, MathUtility::ActivationFunction activationFunc, double learningRate)
+{
+    for (size_t i = 0; i < numOfNeuronsPerLayer.size(); i++)
+    {
+        if (i == 0)
+        {
+            m_layers.push_back(new Layer(i, numOfNeuronsPerLayer[i], 0, activationFunc, learningRate));
+        }
+        else
+        {
+            m_layers.push_back(new Layer(i, numOfNeuronsPerLayer[i], numOfNeuronsPerLayer[i - 1], activationFunc, learningRate));
+        }
+    }
+}
+
+NeuralNetwork::~NeuralNetwork()
+{
+    for (Layer* l : m_layers)
+        delete l;
+}
+
+void NeuralNetwork::print() const
+{
+    int count = 1;
+    for (const Layer* layer : m_layers)
+    {
+        std::cout << "LAYER " << count++ << std::endl;
+        layer->print();
+        std::cout << "\n\n";
+    }
+}
+
+void NeuralNetwork::feedForward(const std::vector<double>& inputs)
+{
+    for (size_t i = 0; i < m_layers.size(); i++)
+    {
+        if (i == 0)
+        {
+            m_layers[i]->setOutputs(inputs);
+        }
+        else
+        {
+            m_layers[i]->feedForward(m_layers[i - 1]->getOutputs());
+        }
+    }
+}
+
+void NeuralNetwork::backwardsPropagate(const std::vector < double >& targets)
+{
+}
+
+bool NeuralNetwork::setWeights(size_t layerNum, const std::vector<std::vector<double>>& weights)
+{
+    if (layerNum < m_layers.size())
+    {
+        return m_layers[layerNum]->setWeights(weights);
+    }
+
+    return false;
+}
